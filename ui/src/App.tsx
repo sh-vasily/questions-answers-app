d69424e81c11061915e58b08abce62f5657576f7
sh-vasily <vasia.punk.rok@gmail.com>
2022-06-24 10:18:27 +0300

    add logic for question creation

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -4,14 +4,28 @@ import './App.css';
 import Header from './Header';
 import Question from './Question';
 
-let App = ()=>
-    <div className="App">
-      <header className="App-header">
-        <Header></Header>
-        <input placeholder='Вопрос'></input>
-        {Array(10).fill(1).map((item, index) => `Вопрос № ${index+1}`).map(item => <Question text={item} key = {item}/>)}  
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>;
+
+let defaultQuestions = Array(10).fill(1).map((item, index) => `Вопрос № ${index+1}`);
+
+let App = ()=>{
+
+  let  [questions, setQuestions] = React.useState(defaultQuestions);
+  let  [question, setQuestion] = React.useState("");
+
+  return     <div className="App">
+  <header className="App-header">
+    <Header></Header>
+    <input placeholder='Вопрос' onChange={(event: React.FormEvent<HTMLInputElement>) => {
+      setQuestion(event.currentTarget.value);
+    }}></input>
+    <button onClick = {(event) => {
+      questions.push(question);
+      setQuestions(questions);
+    }}>Задать вопрос</button>
+    {questions.map(item => <Question text={item} key = {item}/>)}  
+    <img src={logo} className="App-logo" alt="logo" />
+  </header>
+</div>;
+};
 
 export default App;
